2439b059e19e717fcb1a1895abba0d94ee882692
System <none>
2017-07-25 20:30:57 +0000

    Checkpoint

--- app/timestamp.js
+++ app/timestamp.js
@@ -1,5 +1,10 @@
 module.exports = function(app){
   
+  var json = {
+    "unix": null,
+    "natural": null
+  }
+  
   app.route('/', function(req, res){
     res.sendFile(process.cwd()+'views/index.html');
   });
@@ -7,12 +12,28 @@ module.exports = function(app){
   app.route('/:query', function(req, res){
     var query = req.params.query;
     
+    if (!Number(query) && new Date(query) == 'Invalid Date'){
+      res.send(json);
+    }
+    
     if (Number(query)) {
       var unix = query;
       unixToNatural(query);
     }
     
-    else 
+    else {
+      var natural = query;
+      naturalToUnix(query);
+    }
+    
+    function unixToNatural(query){
+      
+    }
+    
+    function naturalToUnix(query){
+      var unix = query.getTime();
+    }
+    
     
   });
   
